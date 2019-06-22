4a619b895fc0b049096c05ad1c7a01cb19df747c
Blake <blake.enyart@gmail.com>
2019-06-22 12:17:59 -0600

    Add find for single game

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -2,12 +2,29 @@ var express = require("express");
 var router = express.Router();
 var Game = require('../../../models').Game;
 
-/* GET all games */
+/* GET all games - INDEX */
 router.get("/", function(req, res, next) {
   Game.findAll()
     .then(games => {
       res.setHeader("Content-Type", "application/json");
       res.status(200).send(JSON.stringify(games));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+
+/* GET one game - SHOW */
+router.get("/:id", function(req, res, next) {
+  Game.findAll({
+    where: {
+      id: req.params.id // request params access
+    }
+  })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(JSON.stringify(game));
     })
     .catch(error => {
       res.setHeader("Content-Type", "application/json");
