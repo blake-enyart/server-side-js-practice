7e4b5a35c58fc18b53fff543d69adf383104218f
Blake <blake.enyart@gmail.com>
2019-06-22 13:33:42 -0600

    Add create route to project

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -15,16 +15,34 @@ router.get("/", function(req, res, next) {
     });
 });
 
-/* GET one game - SHOW */
+/* GET game - SHOW */
 router.get("/:id", function(req, res, next) {
   Game.findAll({
-    where: {
-      id: req.params.id // request params access
-    }
-  })
+      where: {
+        id: req.params.id // request params access
+      }
+    })
     .then(game => {
       res.setHeader("Content-Type", "application/json");
       res.status(200).send(JSON.stringify(game));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+});
+
+/* POST game - CREATE */
+router.post("/", function(req, res, next) {
+  Game.create({
+          title: req.body.title,
+          price: req.body.price,
+          releaseYear: req.body.releaseYear,
+          active: req.body.active
+    })
+    .then(game => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(game));
     })
     .catch(error => {
       res.setHeader("Content-Type", "application/json");
